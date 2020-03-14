14aa891b8b38cd188c254f6b6c74fc890b278d1d
jlgerber <jlgerber@gmail.com>
2020-03-14 13:44:55 -0700

    moves machinery from bin/server.rs to src/service.rs

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -6,25 +6,6 @@ use packybara_grpc::{PackybaraServer, PackybaraService};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (client, connection) = tokio_postgres::connect(
-        "host=127.0.0.1 user=postgres  dbname=packrat password=example port=5432",
-        NoTls,
-    )
-    .await?;
-
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    let addr = "[::1]:50051".parse()?;
-    let packy = PackybaraService::new(client);
-
-    Server::builder()
-        .add_service(PackybaraServer::new(packy))
-        .serve(addr)
-        .await?;
-
+    PackybaraService::run().await?;
     Ok(())
 }
--- src/service.rs
+++ src/service.rs
@@ -1,3 +1,5 @@
+use tokio_postgres::NoTls;
+use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 
 use packybara::coords::Coords as PCoords;
@@ -5,7 +7,7 @@ use packybara::db::find::versionpins::FindVersionPinsRow;
 use packybara::db::traits::*;
 use packybara::packrat::{Client, PackratDb};
 
-use crate::{Coords, Packybara, VersionPinQueryReply, VersionPinQueryRequest};
+use crate::{Coords, Packybara, PackybaraServer, VersionPinQueryReply, VersionPinQueryRequest};
 #[derive(Debug)]
 pub struct PackybaraService {
     client: Client,
@@ -14,6 +16,27 @@ pub struct PackybaraService {
 impl PackybaraService {
     pub fn new(client: Client) -> Self {
         Self { client }
+    }
+    // TODO:: Add configuration as run argument
+    pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
+        let (client, connection) = tokio_postgres::connect(
+            "host=127.0.0.1 user=postgres  dbname=packrat password=example port=5432",
+            NoTls,
+        )
+        .await?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+        let addr = "[::1]:50051".parse()?;
+        let packy = PackybaraService::new(client);
+        Server::builder()
+            .add_service(PackybaraServer::new(packy))
+            .serve(addr)
+            .await?;
+
+        Ok(())
     }
 
     pub fn client(&self) -> &Client {
