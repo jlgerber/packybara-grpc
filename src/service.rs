c077207bfd794a553f2de5e117ddd8ef63fe42e5
jlgerber <jlgerber@gmail.com>
2020-03-17 14:20:30 -0700

    PackybaraService.run takes GrpcUrl input

--- src/service.rs
+++ src/service.rs
@@ -7,7 +7,9 @@ use packybara::db::find::versionpins::FindVersionPinsRow;
 use packybara::db::traits::*;
 use packybara::packrat::{Client, PackratDb};
 
-use crate::{Coords, Packybara, PackybaraServer, VersionPinQueryReply, VersionPinQueryRequest};
+use crate::{
+    url::GrpcUrl, Coords, Packybara, PackybaraServer, VersionPinQueryReply, VersionPinQueryRequest,
+};
 #[derive(Debug)]
 pub struct PackybaraService {
     client: Client,
@@ -31,7 +33,7 @@ impl PackybaraService {
     ///     Ok(())
     /// }
     /// ```
-    pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn run(url: GrpcUrl) -> Result<(), Box<dyn std::error::Error>> {
         let (client, connection) = tokio_postgres::connect(
             "host=127.0.0.1 user=postgres  dbname=packrat password=example port=5432",
             NoTls,
@@ -42,7 +44,7 @@ impl PackybaraService {
                 eprintln!("connection error: {}", e);
             }
         });
-        let addr = "[::1]:50051".parse()?;
+        let addr = url.to_socket_addr()?; //"[::1]:50051".parse()?;
         let packy = PackybaraService::new(client);
         Server::builder()
             .add_service(PackybaraServer::new(packy))
