39cdb96843ee9b8b9372252d333b9bb97f997a97
jlgerber <jlgerber@gmail.com>
2020-03-17 14:21:42 -0700

    bin/server uses UrlBuilder to construct url for PackybaraService::run

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,9 +1,13 @@
 use tokio;
 
-use packybara_grpc::PackybaraService;
+use packybara_grpc::{url_builder, url_builder::UrlBuilder, PackybaraService};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    PackybaraService::run().await?;
+    let url = UrlBuilder::new()
+        .host(url_builder::Host::Localhost)
+        .port(50051)
+        .build();
+    PackybaraService::run(url).await?;
     Ok(())
 }
